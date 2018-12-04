587d03e4be1d80723260f5f61159e27dd42a6177
ericaandrews <eureka.web.dev@gmail.com>
2018-12-04 05:01:30 -0800

    Add files via upload

---
+++ mobile_web/homework4/service-worker.js
@@ -0,0 +1,10 @@
+function setBG(val){
+    if(val == "lightblue"){
+      document.body.style.backgroundImage= "url('lightblue.jpg')"; 
+      document.body.style.backgroundSize="cover";    
+    }
+    if(val == "lightgold"){
+      document.body.style.backgroundImage= "url('lightgold.jpg')";
+      document.body.style.backgroundSize="cover";    
+    }
+}
